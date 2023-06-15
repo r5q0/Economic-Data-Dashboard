71b518b4ac2d334d5d4e5cacbd83f23b132d8702
raq0 <122687779+raq0@users.noreply.github.com>
2023-06-16 01:25:29 +0200

    added favicon

--- index.php
+++ index.php
@@ -1,19 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <link rel="shortcut icon" href="favicon.svg" type="image/x-icon">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Economic Data Dashboard</title>
     <link rel="stylesheet" href="style.css">
-</head >
-<body>
-<?php include_once "Data.php"; getData();?>
+</head>
 
-<center><<a href="index.php"><button><h1>Refresh</h1></button></a>/center>
-    
+<body>
+    <?php include_once "Data.php";
+    getData(); ?>
+
+    <center>
+        <<a href="index.php"><button>
+                <h1>Refresh</h1>
+            </button></a>/center>
+
 
 
 
 
 </body>
+
 </html>
